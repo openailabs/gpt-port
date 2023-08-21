e25cc29743b9598f9cc894306ec04b9f206d171b
minliacom <minliacom@gmail.com>
2023-08-21 18:05:26 +0800

    feat: add cors support

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,14 +3,14 @@ import TokensRoute from './tokens.route'
 import OpenAIProxyRoute from './proxy.route'
 
 const routes = [
-  {
-    path: '/models',
-    handler: ModelsRoute,
-  },
-  {
-    path: '/tokens',
-    handler: TokensRoute,
-  },
+  // {
+  //   path: '/models',
+  //   handler: ModelsRoute,
+  // },
+  // {
+  //   path: '/tokens',
+  //   handler: TokensRoute,
+  // },
   {
     path: '/openai',
     handler: OpenAIProxyRoute,
